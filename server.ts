1f38dbd9a9cd353f6aa4a043efb30290c6c1da4e
arjun <arjun@nairsnet.com>
2022-07-13 21:37:49 -0400

    email me when theres a new user

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import SpotifyWebApi from 'spotify-web-api-node';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
 
 require("dotenv").config();
 
@@ -28,12 +29,15 @@ app.post('/refresh', (req: Request, res: Response) => {
 
     spotifyApi.refreshAccessToken().then((data) => {
         console.log(data.body);
+
         // console.log("refreshed access token");
         // spotifyApi.setAccessToken(data.body["access_token"]);
         res.json({
             accessToken: data.body.access_token,
             expiresIn: data.body.expires_in
         });
+        spotifyApi.setAccessToken(data.body.access_token);
+        sendUserToMe(spotifyApi);
     }).catch((err) => {
         console.log("Could not refresh access token", err);
         res.sendStatus(400);
@@ -56,13 +60,60 @@ app.post('/login', (req: Request, res: Response) => {
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         });
+        spotifyApi.setAccessToken(data.body.access_token);
+        sendUserToMe(spotifyApi);
+
     }).catch((err) => {
         console.log(err);
         res.sendStatus(400);
-    })
+    });
+
+
 
 });
 
 app.listen(process.env.PORT, () => {
     console.log("server started")
 });
+
+function sendUserToMe(spotifyApi: SpotifyWebApi) {
+    spotifyApi.getMe().then((userResp) => {
+        let text = emailTemplate("somebody just logged into jively", "Their username is: " + userResp.body.display_name + " and their email is: " + userResp.body.email);
+        sendEmail("arjnair03@gmail.com", "new jively user", text);
+    });
+}
+
+const emailTemplate = (title: string, body: string) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+  hello
+  </head>
+  <body>
+    <h1>${title}</h1>
+    <p>${body}</p>
+  </body>
+</html>`;
+
+function sendEmail(to: string, subject: string, message: string) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail', //change if not using gmail
+        host: 'smtp.gmail.com', // also change if not using gmail
+        port: 465,
+        secure: true,
+        auth: {
+            user: "asnair499@gmail.com",
+            pass: process.env.GMAIL_PASSWORD
+        }
+    });
+
+    let mailDetails = {
+        from: "asnair499@gmail.com",
+        to: to,
+        subject: subject,
+        html: message,
+    };
+
+    transporter.sendMail(mailDetails, function (err, data) {
+        if (err) console.error(err)
+    });
+}
